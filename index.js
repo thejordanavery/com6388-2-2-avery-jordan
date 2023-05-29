31b7280e8ee2e8b899eee868a302f7a32036a4a7
Jordan Avery <jordanavery@ufl.edu>
2023-05-28 21:32:58 -0400

    first attempt

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Your code here
+function greet() {
+    var username = prompt ("What is your name?") 
+        alert (Hello,' + name);
+
+    var age = prompt ("How old are you?")
+    console.log(parseInt(age)); 
+
+
+    var age = prompt ("How old are you?")
+    var dob = confirm ("Did you celebrate your birthday already? yes/no")
+    if (dob) {
+        alert ('result is ' + new Date().getFullYear() - age")
+    } else {
+        alert ('result is ' + new Date().getFullYear() - age - 1)
+    }
+}
+
+
+   
+    
+
+
+
+
